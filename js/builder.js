706d6f45c78cbed0d02450146ddc340ef6d5fea6
doom99987 <126832091+doom99987@users.noreply.github.com>
2026-06-01 13:06:06 -0700

    handa

--- js/builder.js
+++ js/builder.js
@@ -2675,7 +2675,7 @@ const BOSS_DATA = {
   },
   "Handaconda": {
     hp: 9000,
-    hpVariants: { "Corrupted": 13500 },
+    hpVariants: { "Corrupted": 27000 },
     res: { Physical: 0.50, Holy: 0.50, Arcane: 0.50, Fire: 1.25 },
   },
   "Shadeblade": {
